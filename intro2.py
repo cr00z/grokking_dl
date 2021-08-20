90c0b48ea100a2562748ccaff9b22565d9b21d34
cr00z <netrebinr@gmail.com>
2021-08-20 17:03:02 +0200

    add intro2 script

---
+++ intro2.py
@@ -0,0 +1,24 @@
+WEIGHTS = [0.1, 0.2, 0]
+
+
+def w_sum(add1, add2):
+    # result = 0
+    # for i in range(len(add1)):
+    #     result += add1[i] * add2[i]
+    # return result
+    assert (len(add1) == len(add2))
+    return sum(map(lambda x, y: x * y, add1, add2))
+
+
+def neural_network(input_data, weight):
+    prediction = w_sum(input_data, weight)
+    return prediction
+
+
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+input_data = [toes[0], wlrec[0], nfans[0]]
+pred = neural_network(input_data, WEIGHTS)
+print(pred)
