c5b73898e06b3f86628da7acf3fbfbb484007c32
cr00z <netrebinr@gmail.com>
2021-09-09 11:21:26 +0200

    make some experiments

--- part6/deep_street.py
+++ part6/deep_street.py
@@ -2,8 +2,6 @@ import numpy as np
 
 
 np.random.seed(1)
-
-
 NUM_OF_ITERATIONS = 60
 
 
@@ -22,6 +20,10 @@ streetlights = np.array([[1, 0, 1],
                          [0, 0, 1],
                          [1, 1, 1]])
 walk_vs_stop = np.array([1, 1, 0, 0]).T
+#weights_0_1 = 2 * np.random.random((3, hidden_size)) - 1
+#weights_1_2 = 2 * np.random.random((hidden_size, 1)) - 1
+#weights_0_1 = (np.random.random((3, hidden_size)) > 0.5) - 0.6
+#weights_1_2 = (np.random.random((hidden_size, 1)) > 0.5) - 0.6
 weights_0_1 = 2 * np.random.random((3, hidden_size)) - 1
 weights_1_2 = 2 * np.random.random((hidden_size, 1)) - 1
 
@@ -37,7 +39,17 @@ for iteration in range(NUM_OF_ITERATIONS):
         layer_2_delta = walk_vs_stop[i:i+1] - layer_2
         layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
 
-        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
-        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
-    if iteration % 10 == 9:
-        print("Error: {}".format(layer_2_error))
+        weights_1_2_delta = layer_1.T.dot(layer_2_delta)
+        weights_0_1_delta = layer_0.T.dot(layer_1_delta)
+
+        print("layer_0: {}, layer_1: {}, layer_2: {}".format(layer_0, layer_1, layer_2))
+        print("weight_1_2: {}, weight_0_1: {}".format(weights_1_2, weights_0_1))
+        print("layer_2_delta: {}, layer_1_delta: {}".format(layer_2_delta, layer_1_delta))
+        print("tw_1_2_delta: {}, w_0_1_delta: {}".format(weights_1_2_delta, weights_0_1_delta))
+        print()
+
+        weights_1_2 += alpha * weights_1_2_delta
+        weights_0_1 += alpha * weights_0_1_delta
+        input()
+    #if iteration % 10 == 9:
+    #    print("Error: {}".format(layer_2_error))
--- part6/streetlights.py
+++ part6/streetlights.py
@@ -4,20 +4,24 @@ alpha = 0.1
 streetlights = np.array([[1, 0, 1],
                          [0, 1, 1],
                          [0, 0, 1],
-                         [1, 1, 1],
-                         [0, 1, 1],
-                         [1, 0, 1]])
-walk_vs_stop = np.array([0, 1, 0, 1, 1, 0]).T
+                         [1, 1, 1],])
+                         #[0, 1, 1],
+                         #[1, 0, 1]])
+#walk_vs_stop = np.array([0, 1, 0, 1, 1, 0]).T
+walk_vs_stop = np.array([1, 1, 0, 0]).T
 
-for iteration in range(40):
+for iteration in range(100):
     error_for_all_lights = 0
     for row_index in range(len(walk_vs_stop)):
-        input = streetlights[row_index]
+        inputs = streetlights[row_index]
+        old_weight = weights
         goal_prediction = walk_vs_stop[row_index]
-        prediction = input.dot(weights)
+        prediction = inputs.dot(weights)
         error = (prediction - goal_prediction) ** 2
         error_for_all_lights += error
         delta = prediction - goal_prediction
-        weights = weights - (alpha * input * delta)
-    print("Error: {}, Weights: {}"
-          .format(error_for_all_lights, weights))
+        weights_delta = alpha * inputs * delta
+        weights = weights - weights_delta
+        print("input: {}, weight: {}, Pred: {}, delta: {}, weight_d: {}, weights: {}"
+              .format(inputs, old_weight, prediction, delta, weights_delta, weights))
+    #a = int(input())
